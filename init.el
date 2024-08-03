40d1cebc6c320080086d21ec2fb6fdce3f458065
Naoya Yamashita <conao3@gmail.com>
2024-08-04 05:09:37 +0900

    update

--- init.el
+++ init.el
@@ -25,7 +25,7 @@
 
 (eval-and-compile
   (customize-set-variable
-   'package-archives '(("gnu"   . "https://elpa.gnu.org/packages/")
+   'package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
                        ("melpa" . "https://melpa.org/packages/")))
   (package-initialize)
   (use-package leaf :ensure t)
@@ -37,25 +37,16 @@
     :config
     (leaf-keywords-init)))
 
-;; ここにいっぱい設定を書く
-
 (leaf leaf-convert
   :doc "Convert many format to leaf format"
-  :req "emacs-26.1" "leaf-3.6.0" "leaf-keywords-1.1.0" "ppp-2.1"
-  :tag "tools" "emacs>=26.1"
-  :url "https://github.com/conao3/leaf-convert.el"
-  :added "2024-07-13"
-  :emacs>= 26.1
   :ensure t)
 
 (leaf cus-edit
   :doc "tools for customizing Emacs and Lisp packages"
-  :tag "builtin" "faces" "help"
   :custom `((custom-file . ,(locate-user-emacs-file "custom.el"))))
 
 (leaf cus-start
   :doc "define customization properties of builtins"
-  :tag "builtin" "internal"
   :preface
   (defun c/redraw-frame nil
     (interactive)
@@ -81,7 +72,7 @@
             (truncate-lines . t)
             (use-dialog-box . nil)
             (use-file-dialog . nil)
-            (menu-bar-mode . t)
+            (menu-bar-mode . nil)
             (tool-bar-mode . nil)
             (scroll-bar-mode . nil)
             (indent-tabs-mode . nil))
@@ -91,22 +82,18 @@
 
 (leaf autorevert
   :doc "revert buffers when files on disk change"
-  :tag "builtin"
   :global-minor-mode global-auto-revert-mode)
 
 (leaf delsel
   :doc "delete selection if you insert"
-  :tag "builtin"
   :global-minor-mode delete-selection-mode)
 
 (leaf paren
   :doc "highlight matching paren"
-  :tag "builtin"
   :global-minor-mode show-paren-mode)
 
 (leaf simple
   :doc "basic editing commands for Emacs"
-  :tag "builtin" "internal"
   :custom ((kill-read-only-ok . t)
            (kill-whole-line . t)
            (eval-expression-print-length . nil)
@@ -114,7 +101,6 @@
 
 (leaf files
   :doc "file input and output commands for Emacs"
-  :tag "builtin"
   :global-minor-mode auto-save-visited-mode
   :custom `((auto-save-file-name-transforms . '((".*" ,(locate-user-emacs-file "backup/") t)))
             (backup-directory-alist . '((".*" . ,(locate-user-emacs-file "backup"))
@@ -125,40 +111,26 @@
 
 (leaf startup
   :doc "process Emacs shell arguments"
-  :tag "builtin" "internal"
   :custom `((auto-save-list-file-prefix . ,(locate-user-emacs-file "backup/.saves-"))))
 
 (leaf savehist
   :doc "Save minibuffer history"
-  :tag "builtin"
-  :added "2024-07-13"
   :custom `((savehist-file . ,(locate-user-emacs-file "savehist")))
   :global-minor-mode t)
 
 (leaf flymake
   :doc "A universal on-the-fly syntax checker"
-  :tag "builtin"
-  :added "2024-06-13"
   :bind ((prog-mode-map
           ("M-n" . flymake-goto-next-error)
           ("M-p" . flymake-goto-prev-error))))
 
 (leaf which-key
   :doc "Display available keybindings in popup"
-  :req "emacs-25.1"
-  :tag "emacs>=25.1"
-  :added "2024-07-14"
-  :emacs>= 25.1
   :ensure t
   :global-minor-mode t)
 
 (leaf exec-path-from-shell
   :doc "Get environment variables such as $PATH from the shell"
-  :req "emacs-24.4"
-  :tag "environment" "unix" "emacs>=24.4"
-  :url "https://github.com/purcell/exec-path-from-shell"
-  :added "2024-07-13"
-  :emacs>= 24.4
   :ensure t
   :defun (exec-path-from-shell-initialize)
   :custom ((exec-path-from-shell-check-startup-files)
@@ -168,31 +140,16 @@
 
 (leaf vertico
   :doc "VERTical Interactive COmpletion"
-  :req "emacs-27.1" "compat-30"
-  :tag "completion" "matching" "files" "convenience" "emacs>=27.1"
-  :url "https://github.com/minad/vertico"
-  :added "2024-07-14"
-  :emacs>= 27.1
   :ensure t
   :global-minor-mode t)
 
 (leaf marginalia
   :doc "Enrich existing commands with completion annotations"
-  :req "emacs-27.1" "compat-30"
-  :tag "completion" "matching" "help" "docs" "emacs>=27.1"
-  :url "https://github.com/minad/marginalia"
-  :added "2024-07-14"
-  :emacs>= 27.1
   :ensure t
   :global-minor-mode t)
 
 (leaf consult
   :doc "Consulting completing-read"
-  :req "emacs-27.1" "compat-30"
-  :tag "completion" "files" "matching" "emacs>=27.1"
-  :url "https://github.com/minad/consult"
-  :added "2024-07-14"
-  :emacs>= 27.1
   :ensure t
   :hook (completion-list-mode-hook . consult-preview-at-point-mode)
   :defun consult-line
@@ -228,11 +185,6 @@
 
 (leaf affe
   :doc "Asynchronous Fuzzy Finder for Emacs"
-  :req "emacs-27.1" "consult-1.0"
-  :tag "completion" "files" "matching" "emacs>=27.1"
-  :url "https://github.com/minad/affe"
-  :added "2024-07-14"
-  :emacs>= 27.1
   :ensure t
   :custom ((affe-highlight-function . 'orderless-highlight-matches)
            (affe-regexp-function . 'orderless-pattern-compiler))
@@ -241,11 +193,6 @@
 
 (leaf orderless
   :doc "Completion style for matching regexps in any order"
-  :req "emacs-27.1"
-  :tag "extensions" "emacs>=27.1"
-  :url "https://github.com/oantolin/orderless"
-  :added "2024-07-14"
-  :emacs>= 27.1
   :ensure t
   :custom ((completion-styles . '(orderless))
            (completion-category-defaults . nil)
@@ -253,11 +200,6 @@
 
 (leaf embark-consult
   :doc "Consult integration for Embark"
-  :req "emacs-27.1" "compat-29.1.4.0" "embark-1.0" "consult-1.0"
-  :tag "convenience" "emacs>=27.1"
-  :url "https://github.com/oantolin/embark"
-  :added "2024-07-14"
-  :emacs>= 27.1
   :ensure t
   :bind ((minibuffer-mode-map
           :package emacs
@@ -266,11 +208,6 @@
 
 (leaf corfu
   :doc "COmpletion in Region FUnction"
-  :req "emacs-27.1" "compat-30"
-  :tag "text" "completion" "matching" "convenience" "abbrev" "emacs>=27.1"
-  :url "https://github.com/minad/corfu"
-  :added "2024-07-14"
-  :emacs>= 27.1
   :ensure t
   :global-minor-mode global-corfu-mode corfu-popupinfo-mode
   :custom ((corfu-auto . t)
@@ -282,21 +219,23 @@
 
 (leaf cape
   :doc "Completion At Point Extensions"
-  :req "emacs-27.1" "compat-29.1.4.4"
-  :tag "text" "completion" "matching" "convenience" "abbrev" "emacs>=27.1"
-  :url "https://github.com/minad/cape"
-  :added "2024-07-15"
-  :emacs>= 27.1
   :ensure t
   :config
   (add-to-list 'completion-at-point-functions #'cape-file))
 
+(leaf eglot
+  :doc "The Emacs Client for LSP servers"
+  :hook ((clojure-mode-hook . eglot-ensure))
+  :custom ((eldoc-echo-area-use-multiline-p . nil)
+           (eglot-connect-timeout . 600)))
+
+(leaf eglot-booster
+  :when (executable-find "emacs-lsp-booster")
+  :vc ( :url "https://github.com/jdtsmith/eglot-booster")
+  :global-minor-mode t)
+
 (leaf puni
   :doc "Parentheses Universalistic"
-  :req "emacs-26.1"
-  :tag "tools" "lisp" "convenience" "emacs>=26.1"
-  :url "https://github.com/AmaiKinono/puni"
-  :added "2024-06-13"
   :ensure t
   :global-minor-mode puni-global-mode
   :bind (puni-mode-map
@@ -318,30 +257,10 @@
   :config
   (leaf elec-pair
     :doc "Automatic parenthesis pairing"
-    :tag "builtin"
-    :added "2024-06-29"
     :global-minor-mode electric-pair-mode))
-
-(leaf eglot
-  :doc "The Emacs Client for LSP servers"
-  :tag "builtin"
-  :added "2024-07-14"
-  :hook ((clojure-mode-hook . eglot-ensure))
-  :custom ((eldoc-echo-area-use-multiline-p . nil)
-           (eglot-connect-timeout . 600)))
-
-(leaf eglot-booster
-  :when (executable-find "emacs-lsp-booster")
-  :vc ( :url "https://github.com/jdtsmith/eglot-booster")
-  :global-minor-mode t)
 
 (leaf cider
   :doc "Clojure Interactive Development Environment that Rocks"
-  :req "emacs-26" "clojure-mode-5.19" "parseedn-1.2.1" "queue-0.2" "spinner-1.7" "seq-2.22" "sesman-0.3.2" "transient-0.4.1"
-  :tag "cider" "clojure" "languages" "emacs>=26"
-  :url "https://www.github.com/clojure-emacs/cider"
-  :added "2024-07-14"
-  :emacs>= 26
   :ensure t)
 
 (leaf vim-jp-radio
